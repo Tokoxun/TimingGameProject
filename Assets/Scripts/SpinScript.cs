b75c2e524d32952cac72912ce1a2409c53853e3a
Khaw Xun <xunkhaw2@gmail.com>
2024-02-19 17:04:18 +0800

    direction of spin will change randomly

--- Assets/Scripts/SpinScript.cs
+++ Assets/Scripts/SpinScript.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class SpinScript : MonoBehaviour
 {
+    private float dirChangeTime = 2f;
+    public float changeTime;
     public float spinSpeed = -50f;
+    private float[] changeOrNot;
 
+    void Start()
+    {
+        changeOrNot = new float[2];
+        changeOrNot[0] = spinSpeed;
+        changeOrNot[1] = -spinSpeed;
+    }
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(Vector3.forward * spinSpeed * Time.deltaTime);
-    }
+        changeTime += Time.deltaTime;
+        if(changeTime >= dirChangeTime)
+        {
+            spinSpeed = changeOrNot[Random.Range(0, changeOrNot.Length)];
+            changeTime = 0;
+        }
+    }  
 }
