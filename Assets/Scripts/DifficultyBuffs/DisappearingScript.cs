27127c78ad97f2e24864cce3854fb231aba58103
Khaw Xun <xunkhaw2@gmail.com>
2024-02-21 19:49:33 +0800

    A Script that allow the mark to disappear for a certain amount of time

---
+++ Assets/Scripts/DifficultyBuffs/DisappearingScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisappearingScript : MonoBehaviour
+{
+    public float timeToAppear = 2f;
+    public float AppearTime;
+    public float DurOfDisap = 2f;
+    private float DisapTimer;
+    public GameObject mrk;
+
+    // Update is called once per frame
+    void Update()
+    {
+        AppearTime += Time.deltaTime;
+        if(AppearTime >= timeToAppear)
+        {
+            mrk.SetActive(false);
+            DisapTimer += Time.deltaTime;
+            if(DisapTimer >= DurOfDisap)
+            {
+                mrk.SetActive(true);
+                AppearTime = 0;
+                DisapTimer = 0;
+            } 
+        }
+    }
+}
