146321a2869f663eb29ab8f69b142e2ba6f7784a
Khaw Xun <xunkhaw2@gmail.com>
2025-01-21 14:05:52 +0800

    resolving merge conflict

--- Assets/Scripts/DifficultyBuffs/MarkSpdUp.cs
+++ Assets/Scripts/DifficultyBuffs/MarkSpdUp.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 
 public class MarkSpdUp : MonoBehaviour
 {
-    // private float SpdUp = 5f;
+    private float SpdUp = 5f;
     public void markSpdUp()
     {
-<<<<<<< HEAD
         MarkerSpinScript spinningSpeed = Object.FindFirstObjectByType<MarkerSpinScript>();
         if(spinningSpeed != null)
         {
             spinningSpeed.buffedSpnSpd += spinningSpeed.spinSpeed - SpdUp;
         }
-=======
->>>>>>> b4a3efc162bd51d0e3b6537bddb6a280508ba8e9
     }
 }
