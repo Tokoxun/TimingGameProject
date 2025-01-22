d734d9b8b852bd4c4798a77c20f6f5056f5b9b31
Khaw Xun <xunkhaw2@gmail.com>
2025-01-22 13:19:37 +0800

    Updated script to display health points

--- Assets/Scripts/DifficultyBuffs/HealthScript.cs
+++ Assets/Scripts/DifficultyBuffs/HealthScript.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
 
 public class HealthScript : MonoBehaviour
 {
     public float Max_health = 3;
     public float health = 3;
     public float HpRedc = 1;
+    public Text HealthDisplay;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,7 +23,7 @@ public class HealthScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(health);
+        HealthDisplay.text = health.ToString();
         if(health <= 0)
         {
             Time.timeScale = 0;
