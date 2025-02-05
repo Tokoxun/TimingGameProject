8dd8b1fa68e6e4e840692d32951de1db66511cea
Khaw Xun <xunkhaw2@gmail.com>
2025-02-05 10:01:06 +0800

    Changed script condition to activate its effects

--- Assets/Scripts/DifficultyBuffs/ThreeHitMarker.cs
+++ Assets/Scripts/DifficultyBuffs/ThreeHitMarker.cs
@@ -5,35 +5,33 @@ using UnityEngine.UI;
 
 public class ThreeHitMarker : MonoBehaviour
 {
-    public Text RnumImg;
-    public Image RImg;
-    public Sprite TargetImg;
-    private int Rnum = 2;
-    public Image DispR;
-    private bool toggled = false;
+    private bool toggled = true;
 
-    void Start()
+    void Update()
     {
-        RnumImg.text = Rnum.ToString();
-        RImg.sprite = TargetImg;
-    }
-    public void ThreeHitSelect()
-    {
-        if(!toggled)
+        if(toggled)
         {
             DifficultyManager.secondHit = true;
             DifficultyManager.thirdHit = true;
-            DispR.sprite = TargetImg;
-            toggled = true;
-            return;
-        }
-        if(toggled)
-        {
-            DifficultyManager.secondHit = false;
-            DifficultyManager.thirdHit = false;
-            DispR.sprite = null;
-            toggled = false;
-            return;
         }
     }
+    // public void ThreeHitSelect()
+    // {
+    //     if(!toggled)
+    //     {
+    //         DifficultyManager.secondHit = true;
+    //         DifficultyManager.thirdHit = true;
+    //         DispR.sprite = TargetImg;
+    //         toggled = true;
+    //         return;
+    //     }
+    //     if(toggled)
+    //     {
+    //         DifficultyManager.secondHit = false;
+    //         DifficultyManager.thirdHit = false;
+    //         DispR.sprite = null;
+    //         toggled = false;
+    //         return;
+    //     }
+    // }
 }
