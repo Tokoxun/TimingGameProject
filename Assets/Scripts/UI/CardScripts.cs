f546b9311c464872d53b1382850e0727e23b94ff
Khaw Xun <xunkhaw2@gmail.com>
2025-02-05 10:02:07 +0800

    Commented unused lines of code and updated script function

--- Assets/Scripts/UI/CardScripts.cs
+++ Assets/Scripts/UI/CardScripts.cs
@@ -5,42 +5,41 @@ using UnityEngine.UI;
 
 public class CardScripts : MonoBehaviour
 {
-    public Text Rnumber;
-    public Image displayR;
-    public RisksCompiler RisksList;
+    // public Text Rnumber;
+    // public Image displayR;
+    // private Image ImageR;
     public GameObject chosenR;
-    private Image ImageR;
     public bool selected = false;
 
     // Update is called once per frame
     void Update()
     {
-
-        if(chosenR != null)
-        {
-            displayR.sprite = ImageR.sprite;
-        }
+        // if(chosenR != null)
+        // {
+        //     displayR.sprite = ImageR.sprite;
+        // }
     }
 
-    public void LoadingCard()
-    {
-        if(RisksList.Risks.Length >= 1)
-        {
-            displayR.enabled = true;
-            chosenR = RisksList.Risks[Random.Range(0, RisksList.Risks.Length)];
-            ImageR = chosenR.GetComponent<Image>();
-        }
-        if(RisksList.Risks.Length < 1)
-        {
-            displayR.enabled = false;
-        }
-    }
+    // public void LoadingCard()
+    // {
+    //     if(RisksList.Risks.Length >= 1)
+    //     {
+    //         displayR.enabled = true;
+    //         chosenR = RisksList.Risks[Random.Range(0, RisksList.Risks.Length)];
+    //         ImageR = chosenR.GetComponent<Image>();
+    //     }
+    //     if(RisksList.Risks.Length < 1)
+    //     {
+    //         displayR.enabled = false;
+    //     }
+    // }
 
     public void SelectedCard()
     {
         if(!selected)
         {
             selected = true;
+            Instantiate(chosenR, transform.position, transform.rotation);
             Debug.Log(selected);
         }
         else if(selected)
