6d775a791c653b94949675f636e097fc4029d19b
Khaw Xun <xunkhaw2@gmail.com>
2025-02-05 12:36:50 +0800

    Updated preLoadChoice function to adapt changes from selectedFilter[array] to selectedFilter[list]

--- Assets/Scripts/TriggerEffects.cs
+++ Assets/Scripts/TriggerEffects.cs
@@ -8,7 +8,7 @@ public class TriggerEffects : MonoBehaviour
     public GameObject RisksChoice;
     public CardScripts[] cards;
     private GameObject selected;
-    public GameObject[] selectedFilter;
+    public List<GameObject> selectedFilter = new List<GameObject>();
     public RisksCompiler rlist;
     // Start is called before the first frame update
     void Start()
@@ -29,39 +29,43 @@ public class TriggerEffects : MonoBehaviour
 
     public void preLoadChoice()
     {
-        for(int i = 0; i < selectedFilter.Length; i++)
+        if(rlist.Risks.Count > 0)
         {
-            if(rlist.Risks.Length > 0)
+            selected = rlist.Risks[Random.Range(0, rlist.Risks.Count)];
+            for(int a = 0; a != 4; a++)
             {
-                selected = rlist.Risks[Random.Range(0, rlist.Risks.Length)];
-                for(int a = 0; a < selectedFilter.Length; a++)
+                // Debug.Log(a);
+                // Debug.Log(selectedFilter[a]);
+                // Debug.Log(selected);
+                if(selectedFilter.Count == 0)
                 {
-                    // Debug.Log(a);
-                    // Debug.Log(selectedFilter[a]);
-                    // Debug.Log(selected);
-                    if(numcardListLoaded == rlist.Risks.Length)
-                    {
-                        break;
-                    }
-                    if(selectedFilter[a] == selected)
-                    {
-                        selected = rlist.Risks[Random.Range(0, rlist.Risks.Length)];
-                        a = -1;
-                    }
-                    if(a >= selectedFilter.Length - 1)
-                    {
-                        selectedFilter[i] = selected;
-                        numcardListLoaded += 1;
-                        // Debug.Log(numCardLoaded);
-                    }
+                    selectedFilter.Add(selected);
+                    numcardListLoaded += 1;
+                    a = -1;
+                    continue;
                 }
-            }
+                else if(numcardListLoaded == rlist.Risks.Count)
+                {
+                    break;
+                }
+                else if(selectedFilter[a] == selected)
+                {
+                    selected = rlist.Risks[Random.Range(0, rlist.Risks.Count)];
+                    a = -1;
+                }
+                else if(a >= selectedFilter.Count - 1)
+                {
+                     selectedFilter.Add(selected);
+                     numcardListLoaded += 1;
+                     // Debug.Log(numCardLoaded);
+                }
+             }
         }
     }
 
     public void loadCard()
     {
-        for(int c = 0; c < selectedFilter.Length; c++)
+        for(int c = 0; c < selectedFilter.Count; c++)
         {
             cards[c].chosenR = selectedFilter[c];
         }
