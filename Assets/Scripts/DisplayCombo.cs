d0f08882623fe4b9b7bb5b907669e3266f0dbf3d
Khaw Xun <xunkhaw2@gmail.com>
2025-02-13 09:56:58 +0800

    Updated script to display combo when current combo is not 0

--- Assets/Scripts/DisplayCombo.cs
+++ Assets/Scripts/DisplayCombo.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 
 public class DisplayCombo : MonoBehaviour
 {
+    public GameObject ComboDisplay;
     public Text comboNumber;
     private int currentCombo;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,6 +16,14 @@ public class DisplayCombo : MonoBehaviour
     void Update()
     {
         comboNumber.text = currentCombo.ToString();
+        if(currentCombo == 0)
+        {
+            ComboDisplay.SetActive(false);
+        }
+        else if(currentCombo != 0)
+        {
+            ComboDisplay.SetActive(true);
+        }
     }
 
     public void AddCombo()
