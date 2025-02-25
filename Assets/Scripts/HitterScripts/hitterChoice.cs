72e698c77ad918e09b480bddef57ff6189a904d3
Khaw Xun <xunkhaw2@gmail.com>
2025-02-25 12:30:57 +0800

    Added InputAction to script

--- Assets/Scripts/HitterScripts/hitterChoice.cs
+++ Assets/Scripts/HitterScripts/hitterChoice.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class hitterChoice : MonoBehaviour
 {
+    public InputAction currentInput;
     public GameObject enablShortHitter;
     public GameObject enablLongHitter;
     public Collider2D hitPointLong;
