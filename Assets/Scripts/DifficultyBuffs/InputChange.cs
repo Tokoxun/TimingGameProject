ff59a75c6c274d4b720098e53d3bc65217ac532f
Khaw Xun <xunkhaw2@gmail.com>
2025-02-25 14:05:15 +0800

    Changed first string letter to uppercase

--- Assets/Scripts/DifficultyBuffs/InputChange.cs
+++ Assets/Scripts/DifficultyBuffs/InputChange.cs
@@ -10,12 +10,12 @@ public class InputChange : MonoBehaviour
     void Start()
     {
         totalInput = new string[27] 
-        {"a", "b", "c", "d", "e", 
-        "f", "g", "h", "i", "j", 
-        "k", "l", "m", "n", "o", 
-        "p", "q", "r", "s", "t", 
-        "u", "v", "w", "x", "y", 
-        "z", "space"};
+        {"A", "B", "C", "D", "E", 
+        "F", "G", "H", "I", "J", 
+        "K", "L", "M", "N", "O", 
+        "P", "Q", "R", "S", "T", 
+        "U", "V", "W", "X", "Y", 
+        "Z", "Space"};
     }
 
     // Update is called once per frame
