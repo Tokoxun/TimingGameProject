6751bf895de49d10190153977a585c17f0b0d9a0
Khaw Xun <xunkhaw2@gmail.com>
2025-02-27 08:41:24 +0800

    Updated script with a new variable for checking whether to disable pause

--- Assets/Scripts/DifficultyManager.cs
+++ Assets/Scripts/DifficultyManager.cs
@@ -6,6 +6,7 @@ public static class DifficultyManager
 {
     public static float decreaseArwSpd;
     public static bool displayHitterSprite;
+    public static bool disablePause = false;
     public static string currentInput = "Space";
     public static float addButtonCooldown;
     public static float addArwSpd;
