390791b7be5c63629f3d6f7c85a1690651ed5a64
Khaw Xun <xunkhaw2@gmail.com>
2025-02-27 10:04:50 +0800

    Updated total spin speed calculation method with multiplicative method

--- Assets/Scripts/MarkScripts/MarkerSpinScript.cs
+++ Assets/Scripts/MarkScripts/MarkerSpinScript.cs
@@ -1,21 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MarkerSpinScript : MonoBehaviour
 {
     public float spinSpeed = -35f;
     public float buffedSpnSpd;
+    public float totalBuffedSpnSpd;
 
     // Update is called once per frame
     void Update()
     {
-        buffedSpnSpd = DifficultyManager.addMrkSpd;
+        buffedSpnSpd = (DifficultyManager.addMrkSpd - DifficultyManager.decreaseMrkSpd) / 100;
+        totalBuffedSpnSpd = spinSpeed * buffedSpnSpd;
         if(buffedSpnSpd != 0)
         {
-            transform.Rotate(Vector3.forward * (spinSpeed * buffedSpnSpd) * Time.deltaTime);
+            transform.Rotate(Vector3.forward * (spinSpeed + totalBuffedSpnSpd) * Time.deltaTime);
         }
-        if( buffedSpnSpd == 0)
+        if(buffedSpnSpd == 0)
         {
             transform.Rotate(Vector3.forward * spinSpeed * Time.deltaTime);
         }
