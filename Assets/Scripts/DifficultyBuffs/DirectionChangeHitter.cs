ff704f598c800c6c8db9cc75c3bc496a38284352
Khaw Xun <xunkhaw2@gmail.com>
2025-02-27 14:31:22 +0800

    Minor bug fix where script will trigger marker random rotation instead of hitter

--- Assets/Scripts/DifficultyBuffs/DirectionChangeHitter.cs
+++ Assets/Scripts/DifficultyBuffs/DirectionChangeHitter.cs
@@ -5,11 +5,11 @@ public class DirectionChangeHitter : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnEnable()
     {
-        DifficultyManager.rotateMarker = true;
+        DifficultyManager.rotateHitter = true;
     }
 
     private void OnDisable()
     {
-        DifficultyManager.rotateMarker = false;
+        DifficultyManager.rotateHitter = false;
     }
 }
