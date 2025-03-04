09ce21cb8c3b51e5c85c280b4596dc412ffaf7f0
Khaw Xun <xunkhaw2@gmail.com>
2025-03-04 10:42:05 +0800

    Added a new bool variable for triggering disappearing script

--- Assets/Scripts/DifficultyManager.cs
+++ Assets/Scripts/DifficultyManager.cs
@@ -9,6 +9,7 @@ public static class DifficultyManager
     public static bool displayHitterSprite;
     public static bool disablePause = false;
     public static string currentInput = "Space";
+    public static bool mrkDisap = false;
     public static float addMrkRespawn;
     public static float addButtonCooldown;
     public static float addArwSpd;
