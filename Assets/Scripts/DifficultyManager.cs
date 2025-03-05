653a2226941b45661e13eeae07f2ff17e3b85aee
Khaw Xun <xunkhaw2@gmail.com>
2025-03-05 12:35:34 +0800

    Updated script with a bool variable that helps trigger blinking marker effect

--- Assets/Scripts/DifficultyManager.cs
+++ Assets/Scripts/DifficultyManager.cs
@@ -10,6 +10,7 @@ public static class DifficultyManager
     public static bool disablePause = false;
     public static string currentInput = "Space";
     public static bool mrkDisap = false;
+    public static bool mrkBlink = false;
     public static float addMrkRespawn;
     public static float addButtonCooldown;
     public static float addArwSpd;
