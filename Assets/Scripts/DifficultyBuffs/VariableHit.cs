099365ba11da6f4e886ea51f89a994089a5b5e3f
Khaw Xun <xunkhaw2@gmail.com>
2025-03-06 11:45:28 +0800

    Created a script that spawns a random number on top of the marker(not done)

---
+++ Assets/Scripts/DifficultyBuffs/VariableHit.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class VariableHit : MonoBehaviour
+{
+    public Text numberText;
+    private static int variableNum;
+    public Transform markPosition;
+    public Transform centerPointRotation;
+    private float Xscale;
+    private float Yscale;
+
+    void Start()
+    {
+        float screenX = Screen.width;
+        float screenY = Screen.height;
+        Xscale = Mathf.Round(screenX / 11);
+        Yscale = Mathf.Round(screenY / 5);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(DifficultyManager.displayVariableNum)
+        {
+            numberText.enabled = true;
+            numberText.rectTransform.localScale = markPosition.transform.localScale;
+            numberText.rectTransform.localPosition = new Vector2(markPosition.position.x * Xscale, markPosition.position.y * Yscale);
+            numberText.rectTransform.localRotation = centerPointRotation.rotation;
+            numberText.text = variableNum.ToString();
+        }
+        if(variableNum <= 0)
+        {
+            numberText.enabled = false;
+        }
+    }
+
+    public void decreaseNum()
+    {
+        variableNum -= 1;
+    }
+
+    public static void randomVariable()
+    {
+        variableNum = Random.Range(1, 11);
+        Debug.Log(variableNum);
+    } 
+}
