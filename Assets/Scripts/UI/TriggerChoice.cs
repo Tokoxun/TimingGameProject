378ee84019e5813cd0186d0ab616cbc723f4e8bb
Khaw Xun <xunkhaw2@gmail.com>
2025-03-17 10:51:05 +0800

    Removed cards distance calculator from script

--- Assets/Scripts/UI/TriggerChoice.cs
+++ Assets/Scripts/UI/TriggerChoice.cs
@@ -1,13 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class TriggerChoice : MonoBehaviour
 {
-    public GameObject[] cardList;
-    public Transform midPoint;
-    private float Distmove = 215.7f;
     public PointSystem pointsCheck;
     public TriggerEffects loadEffect;
     public GameObject RisksChoice;
@@ -44,30 +38,9 @@ public class TriggerChoice : MonoBehaviour
             Time.timeScale = 0;
             RisksChoice.SetActive(true);
             loadEffect.preLoadChoice();
-            calculatecardListPlacement();
+            loadEffect.calculatecardListPlacement();
             loadEffect.loadCard();
             signal = false;
-        }
-    }
-
-
-
-    public void calculatecardListPlacement()
-    {
-        if(loadEffect.numcardListLoaded > 0)
-        {
-            float Distgap = 432f;
-            float cardListDisplacementX = midPoint.transform.localPosition.x - Distmove * (loadEffect.numcardListLoaded - 1);
-            for(int n = 0; n != loadEffect.numcardListLoaded; n++)
-            {
-                // cardList[0].transform.position = new Vector2(cardListDisplacementX, midPoint.transform.position.y);
-                cardList[n].SetActive(true);
-                cardList[n].transform.localPosition = new Vector2(cardListDisplacementX, 0);
-            }
-            for(int m = 1; m != loadEffect.numcardListLoaded; m++)
-            {
-                cardList[m].transform.localPosition = new Vector2(cardList[m-1].transform.localPosition.x + Distgap, 0);
-            }
         }
     }
 }
