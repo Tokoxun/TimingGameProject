cd95a431b60dd496b658b3474dd711d24310cb97
Khaw Xun <xunkhaw2@gmail.com>
2025-03-20 13:47:09 +0800

    Removed bool variable from script

--- Assets/Scripts/EffectTag.cs
+++ Assets/Scripts/EffectTag.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public abstract class EffectTag : MonoBehaviour
 {
-    public bool activatedEffect = false;
     public abstract void ActivateEffect();
     public abstract void RemoveEffect();
 }
