5cf8e826d5c231ffebcee7ec331156b0fba8976c
Khaw Xun <xunkhaw2@gmail.com>
2025-03-20 10:34:24 +0800

    Script will add gameObject to inventory instead of activating its effect immediately

--- Assets/Scripts/UI/CardScriptsStarter.cs
+++ Assets/Scripts/UI/CardScriptsStarter.cs
@@ -3,19 +3,23 @@ using UnityEngine;
 public class CardScriptsStarter : CardScripts
 {
     public EffectChoiceStarter choosing;
-    public EffectTag chosenStarter;
 
     public new void SelectedCard()
     {
         if(!Chosen)
         {
             Chosen =true;
-            choosing.startEffect = chosenStarter.ActivateEffect;
+            choosing.startEffect = ActivateTag;
         }
         else if(Chosen)
         {
             Chosen = false;
             choosing.startEffect = null;
         }
+    }
+
+    public new void ActivateTag()
+    {
+        playerInventory.selectedTags.Add(chosenR);
     }
 }
