4dfcba8b782c23d48b99e53e07f8514ac8aae56f
Khaw Xun <xunkhaw2@gmail.com>
2025-03-20 10:32:53 +0800

    Added a new abstract function and bool to script

--- Assets/Scripts/EffectTag.cs
+++ Assets/Scripts/EffectTag.cs
@@ -2,5 +2,7 @@ using UnityEngine;
 
 public abstract class EffectTag : MonoBehaviour
 {
+    public bool activatedEffect = false;
     public abstract void ActivateEffect();
+    public abstract void RemoveEffect();
 }
