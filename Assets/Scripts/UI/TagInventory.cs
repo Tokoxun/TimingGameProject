0dcb941960ee4ee3c803d56e788b4d6fba21436d
Khaw Xun <xunkhaw2@gmail.com>
2025-03-20 09:35:35 +0800

    Improved scripts functionality

--- Assets/Scripts/UI/TagInventory.cs
+++ Assets/Scripts/UI/TagInventory.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 public class TagInventory : MonoBehaviour
 {
     public List<GameObject> selectedTags = new List<GameObject>();
+    private int currentTag;
     public TagSlotScript[] tagSlot;
 
+    void Start()
+    {
+        currentTag = 0;
+    }
     void Update()
     {
-        if(selectedTags.Count > 0 && tagSlot != null)
+        if(selectedTags.Count != currentTag && tagSlot != null)
         {
-            for(int t = 0; t < tagSlot.Length; t++)
+            for(int t = 0; t < selectedTags.Count; t++)
             {
                 InfoTag tagInfo = selectedTags[t].GetComponent<InfoTag>();
                 if(tagInfo != null)
                 {
                     tagSlot[t].tagImage = tagInfo.tagImage;
                     tagSlot[t].tagNumber.text = tagInfo.rNumber.ToString();
+                    currentTag = selectedTags.Count;
                 }
             }
         }
