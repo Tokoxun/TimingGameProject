3837d1691176c362476297087ce075b7b25c13fc
Khaw Xun <xunkhaw2@gmail.com>
2025-03-21 09:02:52 +0800

    Changed variable name containing gameObjects EffectTag script

--- Assets/Scripts/UI/TagSlotScript.cs
+++ Assets/Scripts/UI/TagSlotScript.cs
@@ -35,8 +35,8 @@ public class TagSlotScript : MonoBehaviour
 
     public void RemoveTag()
     {
-        EffectTag tagEffect = tags.GetComponent<EffectTag>();
-        tagEffect.RemoveEffect();
+        EffectTag removeTagEffect = tags.GetComponent<EffectTag>();
+        removeTagEffect.RemoveEffect();
         tagStash.selectedTags.Remove(tags);
         tags = null;
         currentTagImage.sprite = emptySlotImage;
