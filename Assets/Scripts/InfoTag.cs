0ba89b0c6475172977ac34691684dc9f3bc55c52
Khaw Xun <xunkhaw2@gmail.com>
2025-03-25 14:03:33 +0800

    Updated tagImage variable name to baseImage and added a few variable that contains sprite

--- Assets/Scripts/InfoTag.cs
+++ Assets/Scripts/InfoTag.cs
@@ -1,9 +1,14 @@
+using System.Collections;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class InfoTag : MonoBehaviour
 {
     public int rNumber;
-    public Image tagImage;
+    public Image baseImage;
+    public Sprite Level_I;
+    public Sprite Level_II;
+    public Sprite Level_III;
     public string Description;
 }
