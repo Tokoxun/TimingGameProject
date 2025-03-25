723fbe45e17d847ef6e4c572a91168d8644b9718
Khaw Xun <xunkhaw2@gmail.com>
2025-03-25 12:55:32 +0800

    Added some variable to script and a abstract void function

--- Assets/Scripts/EffectTag.cs
+++ Assets/Scripts/EffectTag.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public abstract class EffectTag : MonoBehaviour
 {
+    public bool phase = false;
+    public int currentLevel;
+    public int maxLevel;
+    public float enchanceTimer;
+    public float enchanceTime;
     public abstract void ActivateEffect();
     public abstract void RemoveEffect();
+    public abstract void EnchanceEffect();
 }
