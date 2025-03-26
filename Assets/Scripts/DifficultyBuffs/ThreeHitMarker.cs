9374748adfc22b6cfe500a0504263a759099c5ac
Khaw Xun <xunkhaw2@gmail.com>
2025-03-26 09:27:19 +0800

    Updated script to have enchance function

--- Assets/Scripts/DifficultyBuffs/ThreeHitMarker.cs
+++ Assets/Scripts/DifficultyBuffs/ThreeHitMarker.cs
@@ -4,12 +4,21 @@ public class ThreeHitMarker : EffectTag
 {
     public override void ActivateEffect()
     {
+        currentLevel = 0;
         DifficultyManager.secondHit = true;
-        DifficultyManager.thirdHit = true;
+        // DifficultyManager.thirdHit = true;
     }
     public override void RemoveEffect()
     {
+        currentLevel = 0;
         DifficultyManager.secondHit = false;
         DifficultyManager.thirdHit = false;
+    }
+    public override void EnchanceEffect()
+    {
+        if(currentLevel == 1)
+        {
+            DifficultyManager.thirdHit = true;
+        }
     }
 }
