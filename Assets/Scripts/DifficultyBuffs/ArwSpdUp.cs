24e6b6429af4a5b9271438d07ff0f5910e7e41af
Khaw Xun <xunkhaw2@gmail.com>
2025-03-26 10:02:58 +0800

    Updated scripts with enchance effects

--- Assets/Scripts/DifficultyBuffs/ArwSpdUp.cs
+++ Assets/Scripts/DifficultyBuffs/ArwSpdUp.cs
@@ -1,16 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ArwSpdUp : EffectTag
 {
-    private float SpdUp = 5f;
+    private float SpdUp_I;
+    private float SpdUp_II;
     public override void ActivateEffect()
     {
-        DifficultyManager.addArwSpd += SpdUp;
+        currentLevel = 0;
+        enchanceTimer = 0;
+        SpdUp_I = Random.Range(1, 51);
+        DifficultyManager.addArwSpd += SpdUp_I;
     }
     public override void RemoveEffect()
     {
-        DifficultyManager.addArwSpd -= SpdUp;
+        currentLevel = 0;
+        enchanceTimer = 0;
+        DifficultyManager.addArwSpd -= SpdUp_I;
+    }
+    public override void EnchanceEffect()
+    {
+        if(currentLevel == 1)
+        {
+            SpdUp_II = Random.Range(60, 101);
+            DifficultyManager.addArwSpd -= SpdUp_I;
+            DifficultyManager.addArwSpd += SpdUp_II;
+        }
     }
 }
--- Assets/Scripts/DifficultyBuffs/PivotPointMarkerReduce.cs
+++ Assets/Scripts/DifficultyBuffs/PivotPointMarkerReduce.cs
@@ -4,10 +4,22 @@ public class PivotPointMarkerReduce : EffectTag
 {
     public override void ActivateEffect()
     {
+        currentLevel = 0;
+        enchanceTimer = 0;
         MarkerSpinChild3.scdPivotPoint.SetActive(false);
     }
     public override void RemoveEffect()
     {
+        currentLevel = 0;
+        enchanceTimer = 0;
         MarkerSpinChild3.scdPivotPoint.SetActive(true);
+        MarkerSpinChild4.trdPivotPoint.SetActive(true);
+    }
+    public override void EnchanceEffect()
+    {
+        if(currentLevel == 1)
+        {
+            MarkerSpinChild4.trdPivotPoint.SetActive(false);
+        }
     }
 }
