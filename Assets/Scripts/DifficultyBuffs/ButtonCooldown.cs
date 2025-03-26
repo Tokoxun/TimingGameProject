f43384bceeb7c8922cc1b48507d3608277a356b9
Khaw Xun <xunkhaw2@gmail.com>
2025-03-26 10:37:17 +0800

    Updated script with enchance function

--- Assets/Scripts/DifficultyBuffs/ButtonCooldown.cs
+++ Assets/Scripts/DifficultyBuffs/ButtonCooldown.cs
@@ -1,14 +1,42 @@
+using System.Data;
 using UnityEngine;
 
 public class ButtonCooldown : EffectTag
 {
+    private float actualButtonCooldown;
     private float frstCooldown = 2f;
+    private float scdCooldown = 4f;
+    private float trdCooldown = 8f;
     public override void ActivateEffect()
     {
-        DifficultyManager.addButtonCooldown += frstCooldown;
+        currentLevel = 0;
+        enchanceTimer = 0;
+        actualButtonCooldown = frstCooldown;
+        DifficultyManager.addButtonCooldown += actualButtonCooldown;
+        Debug.Log(DifficultyManager.addButtonCooldown);
     }
     public override void RemoveEffect()
     {
-        DifficultyManager.addButtonCooldown -= frstCooldown;
+        currentLevel = 0;
+        enchanceTimer = 0;
+        DifficultyManager.addButtonCooldown -= actualButtonCooldown;
+        Debug.Log(DifficultyManager.addButtonCooldown);
+    }
+    public override void EnchanceEffect()
+    {
+        if(currentLevel == 1)
+        {
+            DifficultyManager.addButtonCooldown -= actualButtonCooldown;
+            actualButtonCooldown = scdCooldown;
+            DifficultyManager.addButtonCooldown += actualButtonCooldown;
+            Debug.Log(DifficultyManager.addButtonCooldown);
+        }
+        else if(currentLevel == 2)
+        {
+            DifficultyManager.addButtonCooldown -= actualButtonCooldown;
+            actualButtonCooldown = trdCooldown;
+            DifficultyManager.addButtonCooldown += actualButtonCooldown;
+            Debug.Log(DifficultyManager.addButtonCooldown);
+        }
     }
 }
