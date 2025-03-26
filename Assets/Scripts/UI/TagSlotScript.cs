4a353833783b89d3c09c9a8522bf51263223b070
Khaw Xun <xunkhaw2@gmail.com>
2025-03-26 09:56:42 +0800

    Removed timer reset from script

--- Assets/Scripts/UI/TagSlotScript.cs
+++ Assets/Scripts/UI/TagSlotScript.cs
@@ -43,7 +43,6 @@ public class TagSlotScript : MonoBehaviour
             // Debug.Log(tagEnchance.phase);
             if(tagEnchance.currentLevel < tagEnchance.maxLevel && tagEnchance.phase)
             {
-                tagEnchance.enchanceTimer = 0;
                 tagEnchance.enchanceTimer += Time.deltaTime;
                 if(tagEnchance.enchanceTimer >= tagEnchance.enchanceTime)
                 {
