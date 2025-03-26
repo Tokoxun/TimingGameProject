ba663525cab978ccd3266fbf0c423f1811c5ca4c
Khaw Xun <xunkhaw2@gmail.com>
2025-03-26 10:35:13 +0800

    Updated effect parameter calculation

--- Assets/Scripts/DifficultyBuffs/ArwSpdUp.cs
+++ Assets/Scripts/DifficultyBuffs/ArwSpdUp.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class ArwSpdUp : EffectTag
 {
+    private float actualSpdUp;
     private float SpdUp_I;
     private float SpdUp_II;
     public override void ActivateEffect()
@@ -9,21 +10,23 @@ public class ArwSpdUp : EffectTag
         currentLevel = 0;
         enchanceTimer = 0;
         SpdUp_I = Random.Range(1, 51);
-        DifficultyManager.addArwSpd += SpdUp_I;
+        actualSpdUp = SpdUp_I;
+        DifficultyManager.addArwSpd += actualSpdUp;
     }
     public override void RemoveEffect()
     {
         currentLevel = 0;
         enchanceTimer = 0;
-        DifficultyManager.addArwSpd -= SpdUp_I;
+        DifficultyManager.addArwSpd -= actualSpdUp;
     }
     public override void EnchanceEffect()
     {
         if(currentLevel == 1)
         {
             SpdUp_II = Random.Range(60, 101);
+            actualSpdUp = SpdUp_II;
             DifficultyManager.addArwSpd -= SpdUp_I;
-            DifficultyManager.addArwSpd += SpdUp_II;
+            DifficultyManager.addArwSpd += actualSpdUp;
         }
     }
 }
