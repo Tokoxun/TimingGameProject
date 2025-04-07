1aba9b11789e9981a430b5c1a0fae0a0dc1016ff
Khaw Xun <xunkhaw2@gmail.com>
2025-04-07 09:49:14 +0800

    Removed points checking from script

--- Assets/Scripts/UI/TriggerChoice.cs
+++ Assets/Scripts/UI/TriggerChoice.cs
@@ -2,10 +2,8 @@ using UnityEngine;
 
 public class TriggerChoice : MonoBehaviour
 {
-    public PointSystem pointsCheck;
     public TriggerEffects loadEffect;
     public GameObject RisksChoice;
-    public int displayChoice = 5;
     public RisksCompiler checkListLength;
     private bool targetListLength = false;
     // Start is called before the first frame update
