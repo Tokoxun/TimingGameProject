93f2e7d48f106101ff81cbcf739c96ee1d72810b
Khaw Xun <xunkhaw2@gmail.com>
2025-04-18 14:20:24 +0800

    Updated script to changed resolution using method instead of update

--- Assets/Scripts/OptionsScript.cs
+++ Assets/Scripts/OptionsScript.cs
@@ -3,17 +3,32 @@ using UnityEngine.UI;
 
 public class OptionsScript : MonoBehaviour
 {
-    public Dropdown displayOption;
+    public Dropdown displayOptions;
     // Update is called once per frame
-    void Update()
+    // void Update()
+    // {
+    //     if(displayOption.value == 0)
+    //     {
+    //         Screen.SetResolution(941, 701, false);
+    //     }
+    //     else if(displayOption.value == 1)
+    //     {
+    //         Screen.SetResolution(1920, 1080, true);
+    //     }
+    // }
+    void Start()
     {
-        if(displayOption.value == 0)
-        {
-            Screen.SetResolution(941, 701, false);
-        }
-        else if(displayOption.value == 1)
+        Screen.SetResolution(941, 701, false);
+    }
+    public void displayOption()
+    {
+        if(displayOptions.value == 1)
         {
             Screen.SetResolution(1920, 1080, true);
+        }
+        if(displayOptions.value == 0)
+        {
+            Screen.SetResolution(941, 701, false);
         }
     }
 }
