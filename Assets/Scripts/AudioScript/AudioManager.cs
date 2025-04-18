2f944c17d82ed299e7d01628be678fd4c9daa081
Khaw Xun <xunkhaw2@gmail.com>
2025-04-18 14:25:10 +0800

    Created a folder for audio scripts

---
+++ Assets/Scripts/AudioScript/AudioManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sounds[] sounds;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
