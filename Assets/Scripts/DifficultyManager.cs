8174df27d94ef5520faa795d4a11f84ec83a5550
Khaw Xun <xunkhaw2@gmail.com>
2025-04-22 10:24:46 +0800

    Current script is no longer static

--- Assets/Scripts/DifficultyManager.cs
+++ Assets/Scripts/DifficultyManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class DifficultyManager
+public class DifficultyManager : MonoBehaviour
 {
     public static bool displayVariableNum = false;
     public static bool rotateMarker = false;
