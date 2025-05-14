785c77dda205df18a5761d9245e307b8b29d486d
Khaw Xun <xunkhaw2@gmail.com>
2025-05-14 11:47:27 +0800

    choiceTrigger variable is set to false at start of the frame

--- Assets/Scripts/UI/PlayerLevel.cs
+++ Assets/Scripts/UI/PlayerLevel.cs
@@ -11,6 +11,7 @@ public class PlayerLevel : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        choiceTrigger = false;
         levelSlider = this.gameObject.GetComponent<Slider>();
     }
 
