9f1ef4a8b17191d69436770f19fe15c7d5a66630
Khaw Xun <xunkhaw2@gmail.com>
2025-05-19 23:19:42 +0800

    Updated script to only display one selected strip

--- Assets/Scripts/UI/EffectChoiceStarter.cs
+++ Assets/Scripts/UI/EffectChoiceStarter.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EffectChoiceStarter : MonoBehaviour
@@ -5,28 +6,41 @@ public class EffectChoiceStarter : MonoBehaviour
     public delegate void StarterTag();
     public StarterTag startEffect;
     public GameObject conButton;
+    private bool choseStart = false;
+    public List<CardScriptsStarter> cardStarter;
 
     void Start()
     {
         gameObject.SetActive(true);
         Time.timeScale = 0;
+        choseStart = false;
     }
     public void ConfirmEffect()
     {
         startEffect();
         gameObject.SetActive(false);
         Time.timeScale = 1;
+        choseStart = true;
     }
 
     void Update()
     {
-        if(startEffect == null)
+        if (startEffect == null)
         {
             conButton.SetActive(false);
         }
-        else if(startEffect != null)
+        else if (startEffect != null)
         {
             conButton.SetActive(true);
+        }
+        if (!choseStart)
+        {
+            if (cardStarter.Count > 1)
+            {
+                cardStarter[0].selectedAnimation.SetBool("selected", false);
+                cardStarter[0].Chosen = false;
+                cardStarter.Remove(cardStarter[0]);
+            }
         }
     }
 }
