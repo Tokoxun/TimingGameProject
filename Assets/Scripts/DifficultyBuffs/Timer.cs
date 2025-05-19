236a614b465c99f6ae9d349197e12be54e8def51
Khaw Xun <xunkhaw2@gmail.com>
2025-05-19 22:58:51 +0800

    Bug fix where timer will not start countdown

--- Assets/Scripts/DifficultyBuffs/Timer.cs
+++ Assets/Scripts/DifficultyBuffs/Timer.cs
@@ -16,16 +16,12 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        TimerDisplay.text = timer.ToString();
+        timer -= timeSpeed * Time.deltaTime;
+        TimerDisplay.text = Mathf.Round(timer).ToString();
         if (timer <= 0)
         {
             timer = 0;
             triggerGameOver.TriggerGameOver();
-        }
-        else if (timer > 0)
-        {
-            timer -= timeSpeed * Time.deltaTime;
-            timer = Mathf.Round(timer);
         }
     }
 }
