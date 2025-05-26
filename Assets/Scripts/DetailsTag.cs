a2daef3663da18dc660a0e35bff9c2d8a95d8773
Khaw Xun <xunkhaw2@gmail.com>
2025-05-26 12:15:19 +0800

    Created a script that acts as a template for tags information

---
+++ Assets/Scripts/DetailsTag.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[System.Serializable]
+public class DetailsTag
+{
+    public string detailsName;
+    public int rNumber;
+    public Sprite tagSprite;
+    public string Description;
+}
