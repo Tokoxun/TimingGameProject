a06ff1f3e7ff712386004b1399b93105b880fff8
Khaw Xun <xunkhaw2@gmail.com>
2025-06-07 09:05:10 +0800

    Created a script that triggers special hitter collider and also help with cooldown

---
+++ Assets/SpScripts/SPHitTriggerScript.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class SPHitTriggerScript : MonoBehaviour
+{
+    public InputAction inputAction;
+    public GameObject hitterObject;
+    [SerializeField] private float cooldownTimer = 0.5f;
+    [SerializeField] private float hitCooldown;
+    public SpHitScript spHitScript;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        inputAction.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inputAction.triggered)
+        {
+            spHitScript.hitPoint.enabled = true;
+        }
+        if (spHitScript.onCooldown)
+        {
+            hitCooldown += Time.deltaTime;
+            if (hitCooldown >= cooldownTimer)
+            {
+                spHitScript.hitterSprite.enabled = true;
+                spHitScript.onCooldown = false;
+                hitCooldown = 0;
+            }
+            else if (hitCooldown < cooldownTimer)
+            {
+                spHitScript.hitPoint.enabled = false;
+                spHitScript.hitterSprite.enabled = false;
+            }
+        }
+    }
+}
