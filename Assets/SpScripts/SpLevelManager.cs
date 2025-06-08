dedab6e7dc0429d714df5529085d3028cddc6049
Khaw Xun <xunkhaw2@gmail.com>
2025-06-08 08:12:18 +0800

    Updated script to check current player points and increases difficulty

--- Assets/SpScripts/SpLevelManager.cs
+++ Assets/SpScripts/SpLevelManager.cs
@@ -4,10 +4,19 @@ public class SpLevelManager : MonoBehaviour
 {
     public float levelDiff;
     public PointSystem pointLevel;
+    public bool startSwitch;
 
+    void Start()
+    {
+        startSwitch = false;
+    }
     // Update is called once per frame
     void Update()
     {
         levelDiff = pointLevel.points / 100;
+        if (pointLevel.points >= 20)
+        {
+            startSwitch = true;
+        }
     }
 }
