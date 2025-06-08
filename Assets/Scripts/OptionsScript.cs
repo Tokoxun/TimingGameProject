00322b8275d2471bba8853cabbf7299e020259e4
Khaw Xun <xunkhaw2@gmail.com>
2025-06-08 18:11:51 +0800

    Updated script to automaticaly save options setting

--- Assets/Scripts/OptionsScript.cs
+++ Assets/Scripts/OptionsScript.cs
@@ -27,50 +27,58 @@ public class OptionsScript : MonoBehaviour
     void Start()
     {
         confirmWarning.SetActive(false);
-        soundEffectSlider.value = AudioManager.SEsetting;
-        displayOptions.value = currentDisplay;
+        soundEffectSlider.value = PlayerPrefs.GetFloat("volume", soundEffectSlider.maxValue);
+        displayOptions.value = PlayerPrefs.GetInt("screenDisplay", 0);
     }
     public void displayOption()
     {
-        if(displayOptions.value == 1)
+        if (displayOptions.value == 1)
         {
             Screen.SetResolution(1920, 1080, true);
             currentDisplay = 1;
+            PlayerPrefs.SetInt("screenDisplay", currentDisplay);
         }
-        if(displayOptions.value == 0)
+        if (displayOptions.value == 0)
         {
             Screen.SetResolution(941, 701, false);
             currentDisplay = 0;
+            PlayerPrefs.SetInt("screenDisplay", currentDisplay);
         }
     }
-    public void Apply()
+    public void volumeValueChanged()
     {
-        confirmWarning.SetActive(false);
+        PlayerPrefs.SetFloat("volume", soundEffectSlider.value);
         AudioManager.SEsetting = soundEffectSlider.value;
-        changesApplied.SetBool("applied", true);
     }
+    // public void Apply()
+    // {
+    //     confirmWarning.SetActive(false);
+    //     AudioManager.SEsetting = soundEffectSlider.value;
+    //     changesApplied.SetBool("applied", true);
+    // }
     public void Revert()
     {
-        confirmWarning.SetActive(false);
+        // confirmWarning.SetActive(false);
         soundEffectSlider.value = soundEffectSlider.maxValue;
         AudioManager.SEsetting = soundEffectSlider.maxValue;
         displayOptions.value = 0;
         displayOption();
     }
-    public void CancelSave()
-    {
-        soundEffectSlider.value = AudioManager.SEsetting;
-        confirmWarning.SetActive(false);
-    }
+    // public void CancelSave()
+    // {
+    //     soundEffectSlider.value = AudioManager.SEsetting;
+    //     confirmWarning.SetActive(false);
+    // }
     public void CloseOption()
     {
-        if(soundEffectSlider.value != AudioManager.SEsetting)
-        {
-            confirmWarning.SetActive(true);
-        }
-        else if(soundEffectSlider.value == AudioManager.SEsetting)
-        {
-            optionMenu.SetActive(false);
-        }
+        // if(soundEffectSlider.value != AudioManager.SEsetting)
+        // {
+        //     confirmWarning.SetActive(true);
+        // }
+        // else if(soundEffectSlider.value == AudioManager.SEsetting)
+        // {
+        //     optionMenu.SetActive(false);
+        // }
+        optionMenu.SetActive(false);
     }
 }
