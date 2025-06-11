510ffe178e15fe41e0b46d212ea8daf035fc21b4
Khaw Xun <xunkhaw2@gmail.com>
2025-06-11 08:46:01 +0800

    Updated script to signal if hitter is turning its direciont

--- Assets/SpScripts/SpSpinScript.cs
+++ Assets/SpScripts/SpSpinScript.cs
@@ -8,10 +8,14 @@ public class SpSpinScript : MonoBehaviour
     private float totalSpinSpeed;
     public float spinSpeed = -100f;
     private float[] changeOrNot;
+    public float signalTime = 1f;
+    private float change;
+    public GameObject[] turnSignal;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         changeOrNot = new float[2];
+        change = spinSpeed;
     }
 
     // Update is called once per frame
@@ -19,15 +23,29 @@ public class SpSpinScript : MonoBehaviour
     {
         totalSpinSpeed = spinSpeed + (spinSpeed * spLevelManager.levelDiff);
         transform.Rotate(Vector3.forward * (totalSpinSpeed * Time.deltaTime));
-        if(changeOrNot != null)
+        if(changeOrNot != null && spLevelManager.hitterSwitch)
         {
             changeOrNot[0] = spinSpeed;
             changeOrNot[1] = -spinSpeed;
             changeTime += Time.deltaTime;
-            if(changeTime >= dirChangeTime)
+            if (dirChangeTime - changeTime <= signalTime && change != spinSpeed)
             {
-                spinSpeed = changeOrNot[Random.Range(0, changeOrNot.Length)];
+                if (spinSpeed > 0)
+                {
+                    turnSignal[1].SetActive(true);
+                }
+                else if (spinSpeed < 0)
+                {
+                    turnSignal[0].SetActive(true);
+                }
+            }
+            if (changeTime >= dirChangeTime)
+            {
+                spinSpeed = change;
+                change = changeOrNot[Random.Range(0, changeOrNot.Length)];
                 changeTime = 0;
+                turnSignal[0].SetActive(false);
+                turnSignal[1].SetActive(false);
             }
         }
     }
