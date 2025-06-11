aebc1702d44d1f2ccfe88317363c16dea0be64cf
Khaw Xun <xunkhaw2@gmail.com>
2025-06-11 09:22:44 +0800

    Updated script to reset signalTime when game starts

--- Assets/SpScripts/SpSpinScript.cs
+++ Assets/SpScripts/SpSpinScript.cs
@@ -14,6 +14,7 @@ public class SpSpinScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        signalTime = 1f;
         changeOrNot = new float[2];
         change = spinSpeed;
     }
