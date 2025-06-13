43a02fe75a92ae560c372b4ff8586158461c0ea6
Khaw Xun <xunkhaw2@gmail.com>
2025-06-13 10:00:23 +0800

    Updated script to randomly trigger point effects

--- Assets/SpScripts/SpMarkerScripts/SpMarkerScript.cs
+++ Assets/SpScripts/SpMarkerScripts/SpMarkerScript.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpMarkerScript : MonoBehaviour
@@ -7,6 +8,7 @@ public class SpMarkerScript : MonoBehaviour
     public bool onHitted;
     public markPurplePoint markPurplePoint;
     public markBlackPoint markBlackPoint;
+    public GameObject[] randomObjects;
     public PointSystem pointsScript;
     public SpMarkerRespawn spMarkerRespawn;
     public Collider2D markCol;
@@ -22,7 +24,7 @@ public class SpMarkerScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if(col.CompareTag("point"))
+        if (col.CompareTag("point"))
         {
             spMarkerRespawn.randomizeCooldown();
             audioManager.Play("MarkHitted");
@@ -34,16 +36,47 @@ public class SpMarkerScript : MonoBehaviour
     }
     public void Respawn()
     {
-        if (markPurplePoint != null && spLevelManager.warningSwitch)
-        {
-            markPurplePoint.StartEffect();
-        }
-        else if (markBlackPoint != null && spLevelManager.mineSwitch)
-        {
-            markBlackPoint.StartEffect();
-        }
+        // if (markPurplePoint != null && spLevelManager.warningSwitch)
+        // {
+        //     markPurplePoint.StartEffect();
+        // }
+        // else if (markBlackPoint != null && spLevelManager.mineSwitch)
+        // {
+        //     markBlackPoint.StartEffect();
+        // }
+        randomPointEffect();
         onHitted = false;
         spMarker.enabled = true;
         markCol.enabled = true;
+    }
+
+    public void randomPointEffect()
+    {
+        int randomNum = Random.Range(0, randomObjects.Length + 1);
+        if (randomNum >= randomObjects.Length)
+        {
+            Debug.Log("nothing");
+            return;
+        }
+        if (randomNum == 0)
+        {
+            if (spLevelManager.warningSwitch)
+            {
+                markPurplePoint purplePoint = randomObjects[randomNum].GetComponent<markPurplePoint>();
+                purplePoint.StartEffect();
+            }
+            Debug.Log("purple");
+            return;
+        }
+        if (randomNum == 1)
+        {
+            if (spLevelManager.mineSwitch)
+            {
+                markBlackPoint blackPoint = randomObjects[randomNum].GetComponent<markBlackPoint>();
+                blackPoint.StartEffect();
+            }
+            Debug.Log("Black");
+            return;
+        }
     }
 }
