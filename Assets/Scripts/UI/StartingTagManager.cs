e721c2a6e394271bba58c901214c717cfccc5d41
Khaw Xun <xunkhaw2@gmail.com>
2025-06-13 11:51:57 +0800

    Fixed a issue where timer and health will not reset when reseting scene

--- Assets/Scripts/UI/StartingTagManager.cs
+++ Assets/Scripts/UI/StartingTagManager.cs
@@ -5,25 +5,33 @@ public class StartingTagManager : MonoBehaviour
     public GameObject equipTimer;
     public GameObject equipHealth;
     public GameObject currentEquip;
-    
     void Update()
     {
-        if(TimerTrigger.startTimer)
+        if (TimerTrigger.startTimer)
         {
             currentEquip = equipTimer;
         }
-        else if(HealthScriptTrigger.startHealth)
+        else if (HealthScriptTrigger.startHealth)
         {
             currentEquip = equipHealth;
         }
-        if(currentEquip != null)
+        if (currentEquip != null)
         {
             currentEquip.SetActive(true);
-            if(!HealthScriptTrigger.startHealth && !TimerTrigger.startTimer)
+            if (!HealthScriptTrigger.startHealth && !TimerTrigger.startTimer)
             {
                 currentEquip.SetActive(false);
                 currentEquip = null;
             }
         }
+    }
+
+    void OnDisable()
+    {
+        TimerTrigger.startTimer = false;
+        HealthScriptTrigger.startHealth = false;
+        equipHealth.SetActive(false);
+        equipTimer.SetActive(false);
+        currentEquip = null;
     }
 }
